630e18b0fe5c56840f28c9c74ba7f4cca6aa4d25
Subhro-ai <subhrajyoti.singha30@gmail.com>
2024-12-21 00:39:28 +0530

    Path Variable

--- src/main/java/net/engineeringdigest/journalApp/controller/JournalEntryController.java
+++ src/main/java/net/engineeringdigest/journalApp/controller/JournalEntryController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,4 +43,8 @@ public class JournalEntryController {
         return journalEntries.remove(myId);
     }
 
+    @PutMapping("/id/{myId}")
+    public JournalEntry deleteJournalEntryById(@PathVariable Long myId, @RequestBody JournalEntry myEntry) {
+        return journalEntries.put(myId, myEntry);
+    }
 }
